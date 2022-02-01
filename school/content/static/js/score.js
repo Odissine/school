d2b841194b5d9a07934ec13725bbd16c893c453d
Cyril <albator74@hotmail.com>
2022-02-01 23:07:02 +0100

    Bug Fix

--- school/content/static/js/score.js
+++ school/content/static/js/score.js
@@ -1,15 +1,14 @@
 function calculPoints(countSecondes, secondes) {
-    countSecondes = countSecondes - secondes;
-        if (countSecondes <= 2) {
+        if (countSecondes <= 1) {
             points = 3;
         }
-        if (countSecondes > 2 && countSecondes <=5) {
+        if (countSecondes > 1 && countSecondes <=2) {
             points = 2;
         }
-        if (countSecondes > 5 && countSecondes <=8) {
+        if (countSecondes > 2 && countSecondes <=4) {
             points = 1;
         }
-        if (countSecondes > 8) {
+        if (countSecondes > 4) {
             points = 0;
         }
     return points;
