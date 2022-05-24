1d71a2cabab953158ed44a1f793e95fbe118b556
Cyril <albator74@hotmail.com>
2022-05-24 17:18:50 +0200

    Sondage V1

--- school/school/conf/development/settings.py
+++ school/school/conf/development/settings.py
@@ -77,16 +77,16 @@ EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'priv.cnf',
-        },
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'OPTIONS': {
+    #         'read_default_file': 'priv.cnf',
+    #     },
+    # }
 }
 
 
